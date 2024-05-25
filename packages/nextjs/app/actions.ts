e79d4ae0df29bc04b1a2ba2e250a7d724b665bdb
koisose <plokotokbelajar@gmail.com>
2024-05-25 09:33:59 +0700

    ✨ feat(nextjs): Add Manta Guessing game to nextjs
    This commit adds a basic Manta guessing game to the Next.js application.
    
    The game uses Gemini AI to generate random words, and players can guess the word to win Manta native tokens. The game also includes a card display with a title and description, and a "Guess" button.
    
    The following features were added:
    
    - A new "Create a New Game" modal that allows users to generate a game with a random word and a clue.
    - A new "Make a Guess" modal that allows users to enter a guess for the generated word.
    - Added the necessary code to integrate Gemini AI to generate random words.
    - Added the necessary code to save the generated game on-chain.
    - Added the necessary code to handle user guesses and award rewards.
    
    This commit also includes a new `actions.ts` file that contains all the logic for interacting with the game.
    
    The UI for the game is still under development, but this commit provides the basic functionality.

---
+++ packages/nextjs/app/actions.ts
@@ -0,0 +1,133 @@
+'use server'
+import FormData from 'form-data';
+import path from 'path';
+import { Deta } from 'deta';
+import {
+  GoogleGenerativeAI, HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+import fetch from 'node-fetch'
+import fs from 'node:fs'
+import { execSync } from 'child_process';
+export async function createImage(prompt: string) {
+  // export async function createImage() {
+
+  const engineId = 'stable-diffusion-v1-6'
+  const apiHost = process.env.API_HOST ?? 'https://api.stability.ai'
+  const apiKey = process.env.STABILITY_API_KEY
+
+  if (!apiKey) throw new Error('Missing Stability API key.')
+
+  // NOTE: This example is using a NodeJS FormData library.
+  // Browsers should use their native FormData class.
+  // React Native apps should also use their native FormData class.
+  const imagePath = path.join(process.cwd(), 'public', 'tshirt.jpg');
+
+  // Read the image file
+  const imageBuffer = fs.readFileSync(imagePath);
+  const formData = new FormData()
+  formData.append('init_image', imageBuffer)
+  formData.append('init_image_mode', 'IMAGE_STRENGTH')
+  formData.append('image_strength', 0.35)
+  formData.append('text_prompts[0][text]', prompt)
+  formData.append('cfg_scale', 7)
+  formData.append('samples', 1)
+  formData.append('steps', 30)
+
+  const response = await fetch(
+    `${apiHost}/v1/generation/${engineId}/image-to-image`,
+    {
+      method: 'POST',
+      headers: {
+        ...formData.getHeaders(),
+        Accept: 'application/json',
+        Authorization: `Bearer ${apiKey}`,
+      },
+      body: formData,
+    }
+  )
+
+  if (!response.ok) {
+    throw new Error(`Non-200 response: ${await response.text()}`)
+  }
+
+
+  const responseJSON = (await response.json())
+  // console.log((responseJSON as any))
+  const oneImage = (responseJSON as any).artifacts[0].base64
+  // return "https://upload.wikimedia.org/wikipedia/commons/c/c6/Grey_Tshirt.jpg"
+  return oneImage
+
+}
+export async function generateTshirt() {
+  const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY as string);
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash-latest",
+    systemInstruction: "you're a tshirt generator create a tshirt title and description return in json like `{\"title\":\"lycra tshirt\",\"description\":\"this tshirt is made with lycra fabric that is flexible and breathable\"}` "
+  },
+    { apiVersion: 'v1beta' });
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 8192,
+    responseMimeType: "application/json",
+  };
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    },
+  ];
+  const chatSession = model.startChat({
+    generationConfig,
+    safetySettings,
+    history: [
+    ],
+  });
+
+  const randomString = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  const result = await chatSession.sendMessage(`generate new random tshirt with unique identifier: ${randomString}`);
+  return result.response.text();
+}
+
+export async function generateImageWithLilypad() {
+  const lilypadPath = path.join(process.cwd(), 'public', 'lilypad');
+  const web3PrivateKey=process.env.WEB3_API_KEY;
+  return execSync(`export WEB3_PRIVATE_KEY=${web3PrivateKey} && ${lilypadPath} run cowsay:v0.0.3 -i Message="moo"`)
+}
+
+
+export async function getDetaData(databaseName: string) {
+  // @ts-ignore
+  const deta = new Deta(process.env.DETA_KEY);
+  const db = deta.Base(databaseName);
+  const data = await db.fetch({})
+  return data;
+}
+
+export async function putData(databaseName: string, data: any) {
+  // @ts-ignore
+  const deta = new Deta(process.env.DETA_KEY);
+  const db = deta.Base(databaseName);
+  await db.put(data);
+  return true
+}
+export async function deleteData(databaseName: string, key: any) {
+  // @ts-ignore
+  const deta = new Deta(process.env.DETA_KEY);
+  const db = deta.Base(databaseName);
+  await db.delete(key);
+}
--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -3,11 +3,31 @@
 import Link from "next/link";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+
 import { Address } from "~~/components/scaffold-eth";
+import { useState,Fragment  } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
+
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [isOpenGuess, setIsOpenGuess] = useState(false);
+  function closeModalGuess() {
+    setIsOpenGuess(false);
+  }
+
+  function openModalGuess() {
+    setIsOpenGuess(true);
+  }
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
 
   return (
     <>
@@ -15,53 +35,201 @@ const Home: NextPage = () => {
         <div className="px-5">
           <h1 className="text-center">
             <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+            <span className="block text-4xl font-bold">Manta Guessing game</span>
           </h1>
           <div className="flex justify-center items-center space-x-2">
             <p className="my-2 font-medium">Connected Address:</p>
             <Address address={connectedAddress} />
           </div>
+
           <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/app/page.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
+            This game is a guessing game every ai generated game is saved onchain if you win, you win a manta native token
+
           </p>
         </div>
 
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contracts
-                </Link>{" "}
-                tab.
-              </p>
+
+        <div className="flex-grow bg-base-300 w-full mt-16 px-8">
+   
+            <div className="flex items-center flex-col flex-grow ">
+             
+              <div className="flex-grow bg-base-300 w-full  px-8 py-5">
+                <h1 className="text-6xl font-bold text-center">Explore</h1>
+                <div className="flex justify-center items-center mt-4">
+                  <button
+                    type="button"
+                    onClick={openModal}
+                    className="px-4 py-2 bg-blue-500 text-white rounded-md mt-5"
+                  >
+                    Create Game
+                  </button>
+                </div>
+                <div className="border rounded-lg p-4 mt-8 inline-block bg-white text-black">
+                  <div className="text-center">
+                    <h2 className="text-2xl font-bold">Card Title</h2>
+                    <p className="mt-2 text-gray-600">Card description goes here.</p>
+                  </div>
+                  <div className="flex justify-center mt-4">
+                    <button
+                      type="button"
+                      onClick={openModalGuess}
+                      className="px-4 py-2 bg-blue-500 text-white rounded-md"
+                    >
+                      Guess
+                    </button>
+                  </div>
+                </div>
+                <Transition appear show={isOpenGuess} as={Fragment}>
+                  <Dialog as="div" className="relative z-10" onClose={closeModalGuess}>
+                    <Transition.Child
+                      as={Fragment}
+                      enter="ease-out duration-300"
+                      enterFrom="opacity-0"
+                      enterTo="opacity-100"
+                      leave="ease-in duration-200"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                    >
+                      <div className="fixed inset-0 bg-black bg-opacity-25" />
+                    </Transition.Child>
+                    <div className="fixed inset-0 overflow-y-auto">
+                      <div className="flex min-h-full items-center justify-center p-4 text-center">
+                        <Transition.Child
+                          as={Fragment}
+                          enter="ease-out duration-300"
+                          enterFrom="opacity-0 scale-95"
+                          enterTo="opacity-100 scale-100"
+                          leave="ease-in duration-200"
+                          leaveFrom="opacity-100 scale-100"
+                          leaveTo="opacity-0 scale-95"
+                        >
+                          <Dialog.Panel className="w-auto transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                            <Dialog.Title
+                              as="h3"
+                              className="text-lg font-medium leading-6 text-black"
+                            >
+                              Make a Guess
+                            </Dialog.Title>
+                            <div className="mt-4">
+                              <input
+                                type="text"
+                                className="w-full px-4 py-2 border border-black rounded-md text-black bg-white"
+                                placeholder="Enter your guess"
+                              />
+                              <div className="flex justify-center mt-4">
+                                <button
+                                  type="button"
+                                  className="px-4 py-2 bg-blue-500 text-white rounded-md"
+                                >
+                                  Guess
+                                </button>
+                              </div>
+                            </div>
+                          </Dialog.Panel>
+                        </Transition.Child>
+                      </div>
+                    </div>
+                  </Dialog>
+                </Transition>
+                <Transition appear show={isOpen} as={Fragment}>
+                  <Dialog as="div" className="relative z-10" onClose={closeModal}>
+                    <Transition.Child
+                      as={Fragment}
+                      enter="ease-out duration-300"
+                      enterFrom="opacity-0"
+                      enterTo="opacity-100"
+                      leave="ease-in duration-200"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                    >
+                      <div className="fixed inset-0 bg-black bg-opacity-25" />
+                    </Transition.Child>
+                    <div className="fixed inset-0 overflow-y-auto">
+                      <div className="flex min-h-full items-center justify-center p-4 text-center">
+                        <Transition.Child
+                          as={Fragment}
+                          enter="ease-out duration-300"
+                          enterFrom="opacity-0 scale-95"
+                          enterTo="opacity-100 scale-100"
+                          leave="ease-in duration-200"
+                          leaveFrom="opacity-100 scale-100"
+                          leaveTo="opacity-0 scale-95"
+                        >
+                          <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                            <Dialog.Title
+                              as="h3"
+                              className="text-lg font-medium leading-6 text-gray-900"
+                            >
+                              Create a New Game
+                            </Dialog.Title>
+                            <div className="mt-2">
+                              <p className="text-sm text-gray-500">
+                               click on generate game to create game, we are using gemini ai to create the word to guess randomly,
+                               then you need to pay manta native gas token to create game anybody who can guess the word, will get 
+                               that manta native token
+                              </p>
+                            </div>
+                            <div className="mt-4">
+                            <div className="flex justify-center my-4">
+                                
+                                <button
+                                  type="button"
+                                  className="inline-flex justify-center rounded-md border border-transparent bg-blue-500 px-4 py-2 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                              
+                                >
+                                  Generate game
+                                </button>
+                              </div>
+                              <div className="flex justify-center my-4">
+                                <div className="rounded-lg bg-gray-200 p-4">
+                                  <p className="text-gray-700 text-center font-bold">
+                                   Generated word:
+                                  </p>
+                                  <p className="text-gray-700 text-center">
+                                   Generated word:
+                                  </p>
+                                </div>
+                              </div>
+                              <div className="rounded-lg bg-gray-200 p-4">
+                              <p className="text-gray-700 text-center font-bold">
+                                   Clue:
+                                  </p>
+                                <p className="text-gray-700">
+                                  This is a generated component with a border radius and grey background.
+                                </p>
+                              </div>
+                            <div className="mt-4">
+                            <p className="text-gray-700">
+                                  How much manta you want to put in this game:
+                                </p>
+                              <input
+                                type="number"
+                                className="w-full rounded-md bg-white p-2 text-gray-700 border border-black"
+                                placeholder="eth"
+                              />
+                            </div>
+                            <div className="flex justify-center my-4">
+                                
+                                <button
+                                  type="button"
+                                  className="inline-flex justify-center rounded-md border border-transparent bg-blue-500 px-4 py-2 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                              
+                                >
+                                  Create game
+                                </button>
+                              </div>
+                            </div>
+                          </Dialog.Panel>
+                        </Transition.Child>
+                      </div>
+                    </div>
+                  </Dialog>
+                </Transition>
+              </div>
             </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-          </div>
+       
+
+
         </div>
       </div>
     </>
